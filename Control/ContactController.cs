8061435f3cde27ed8dfad85534d68f2921b30f03
bondarenkoigor <Bond_fh54@student.itstep.org>
2022-05-16 19:21:22 +0300

    Contact Controller

---
+++ Control/ContactController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MVCPractice.Model;
+
+namespace MVCPractice.Control
+{
+    internal class ContactController
+    {
+        public List<Contact> Contacts { get; set; }
+
+        public ContactController() => Contacts = new List<Contact>();
+
+        public void AddContact() => Contacts.Add(new Contact());
+
+        public void RemoveContact(int ind) => Contacts.RemoveAt(ind);
+    }
+}
