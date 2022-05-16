6dccb3fbf1567d2c60dab11ead93f9304dd3886b
bondarenkoigor <Bond_fh54@student.itstep.org>
2022-05-16 20:28:23 +0300

    ID's added

--- Model/Contact.cs
+++ Model/Contact.cs
@@ -11,14 +11,17 @@ namespace MVCPractice.Model
         public string Name { get; set; }
         public string Surname { get; set; }
         public string Address { get; set; }
-        public List<string> Numbers { get; set; }
+        public List<string> Numbers { get; private set; }
+        public int id { get; private set; }
 
+        private static int idCounter = 0;
         public Contact(string name, string surname, string address, List<string> nums)
         {
             Name = name;
             Surname = surname;
             Address = address;
             Numbers = nums;
+            id = ++idCounter;
         }
         public Contact() : this("Name", "Surname", "Address", new List<string> { "number" }) { }
 
